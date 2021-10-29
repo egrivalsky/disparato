043380b4660521c6bbf2f7d47f20bc21c753d8ae
unknown <erik@computer.com>
2021-10-29 17:25:42 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <View style={styles.titleBar}>
+        <Text style={styles.disparato}>
+          [ d i s p a r a t o ]
+        </Text>
+      </View>
+      <View style={styles.container}>
+        <Button title="Headlines" style={styles.headlinebutton}></Button>
+        <Button title="Manual"></Button>
       <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleBar: {
+    width: '100%',
+    marginTop: 50,
+    justifyContent: 'center',
+    backgroundColor: "lavender",
+    alignItems: 'center',
+  },
+  disparato: {
+    fontSize: 36
+  },
+  headlineButton: {
+    height: '30%',
+    maxHeight: '30%',
+  }
 });
