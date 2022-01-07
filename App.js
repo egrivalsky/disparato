1e9fe9a27db38582a0cdbbc544d4f9be65ab0414
unknown <erik@computer.com>
2022-01-07 16:00:15 -0500

    work in progress

--- App.js
+++ App.js
@@ -12,21 +12,30 @@ export default function App(props) {
   // let [ screenShown, setScreenShown ] = useState(startScreen);
   let [ firstWord, setFirstWord ] = useState('');
   let [ wordTwo, setWordTwo ] = useState('');
-  let wordsArray = [firstWord, 'onion', wordTwo, 'spaghetti'];
-  let [allWords, setAllWords] = useState([])
+  let [firstDegreeWords, setFirstDegreeWords] = useState([])
+  let [wordOneList, setWordOneList] = useState([]);
+  let [wordTwoList, setWordTwoList] = useState([]);
+
 
   const onGoHandler = async (selectedWordOne, selectedWordTwo)=> { 
+
     setFirstWord(selectedWordOne); 
     setWordTwo(selectedWordTwo);
 
+
     try { 
+
       let response = await fetch(`http://192.168.1.184:8000/related_words/${selectedWordOne}/${selectedWordTwo}/data.json`);
 
       let json = await response.json();
-      let firstDegreeWords = json.immediateWords
+
+      let wordOneRelations = json.wordOneSet;
+      let wordTwoRelations = json.wordTwoSet;
+      let firstDegreeWords = json.immediateWords;
       console.log(firstDegreeWords)
-      setAllWords(firstDegreeWords)
-      // return disparato
+      setWordOneList([...wordOneRelations]);
+      setWordTwoList([...wordTwoRelations]);
+      setFirstDegreeWords(firstDegreeWords)
       } catch(error) {
       console.error(error)
       }
@@ -40,7 +49,10 @@ export default function App(props) {
   let wordsScreen = <FoundWords onPressHandler={goBackHandler}
   w1={firstWord} 
   w2={wordTwo}
-  disparato={allWords} />;
+  disparato={firstDegreeWords}
+  w1List={wordOneList}
+  w2List={wordTwoList}
+  />;
 
   let content = startScreen;
 
--- screens/FoundWords.js
+++ screens/FoundWords.js
@@ -1,25 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import {Text, ScrollView, View, StyleSheet, Button} from "react-native"
 import Header from "../components/Header";
 
 
 const FoundWords = (props) => {
     
-    const listOfWords = `${props.w1} meets ${props.w2}:`
+    const w1 = props.w1;
+    const w2 = props.w2;
+
+    const relatedWordsLists = {
+        'wordOneList': props.w1List,
+        'wordTwoList': props.w2List
+    } 
+
+    let [wordOneImmediateRelations, setWordOneImmediateRelations] = useState([]);
+    let [wordTwoImmediateRelations, setWordTwoImmediateRelations] = useState([]);
+
+    const goDeeperPressHandler = async () => {
+        console.log('1')
+        try { 
+            relatedWordsListsString = JSON.stringify(relatedWordsLists)            
+            let response = await fetch(`http://192.168.1.184:8000/second_degree_words/${relatedWordsListsString}`);
+            console.log('2')
+            let json = await response.json();
+            console.log(json.wordOne)
+            console.log('3')
+            // console.log(json)
+            console.log('4')
+            // let firstDegreeWords = json.immediateWords
+            // console.log(firstDegreeWords)
+            // setAllWords(firstDegreeWords)
+            } catch(error) {
+            console.log('5')
+            console.error(error.message)
+            }
+            console.log('6')
+          };
     
     return (
     <View style={styles.screen}>
         <Header />
-        <View style={styles.wordList}>
-        <Text style={styles.listItems}>{listOfWords}</Text>
-        <View>
-            <Text>
-                {props.disparato}
+        <View style={styles.listHeadingContainer}>
+            <Text style={styles.listHeading}>
+                <Text style={styles.searchedWord}>{w1} </Text>
+                <Text>&</Text>
+                <Text style={styles.searchedWord}> {w2}</Text>
             </Text>
         </View>
 
+        <View style={styles.list}>
+        <ScrollView style={styles.scrollView}>
+            {props.disparato.map(word => 
+            <View key={word} style={styles.wordCard}>
+               <Text style={styles.word}>{word}</Text>
+            </View>
+            )}
+        </ScrollView>
         </View>
-        <Button title="[ go back ]" onPress={()=>{props.onPressHandler()}} />
+
+        <View style={styles.buttonsContainer}>                
+           <Button title="[ go back ]" style={styles.button} onPress={()=>{props.onPressHandler()}} />
+           <Button title="[ go deep ]" style={styles.button} onPress={()=>{goDeeperPressHandler()}} />
+        </View>
     </View>
 
 )}
@@ -29,14 +71,54 @@ export default FoundWords;
 
 const styles = StyleSheet.create({
 
-    wordList: {
-        fontSize: 28,
-        padding: 36,
+    listHeadinContainer: {
         justifyContent: 'center',
         alignItems: 'center',
+    },
+
+    listHeading: {
+        fontSize: 24,   
+    },
+    searchedWord: {
+        fontSize: 28,
+    },
+
+    list: {
+        width: '100%',
+        alignItems: 'center',
+        height: '70%',
+        paddingVertical: 10,
 
     },
-    listItems: {
-        fontSize: 28,
-    }
+    scrollView: {
+        paddingHorizontal: 20,
+    },
+    word: {
+        fontSize: 22,
+        paddingHorizontal: 40,
+    },
+    wordCard: {
+        width:'100%',
+        backgroundColor: 'skyblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 2,
+        borderRadius: 25,
+    },
+
+    buttonsContainer: {
+        height: 100,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        paddingBottom: 20,
+    },
+    button: {
+
+    },
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+        // justifyContent: 'space-between'
+
+      }
 })
